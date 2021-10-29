ee6ffd63144907754ea8a68362464ceecbdab126
Piotr Cichocki <s175856@student.pg.edu.pl>
2021-10-29 20:46:54 +0200

    1

--- DRAG/Form1.Designer.cs
+++ DRAG/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace DRAG
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
@@ -58,6 +59,9 @@ namespace DRAG
             this.lblDropHere10 = new System.Windows.Forms.Label();
             this.lblDropHere13 = new System.Windows.Forms.Label();
             this.lblDropHere15 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -82,15 +86,15 @@ namespace DRAG
             this.panel1.Controls.Add(this.pictureBox3);
             this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(12, 23);
+            this.panel1.Location = new System.Drawing.Point(12, 41);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(190, 415);
+            this.panel1.Size = new System.Drawing.Size(182, 369);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = global::DRAG.Properties.Resources.kierownica;
-            this.pictureBox3.Location = new System.Drawing.Point(16, 282);
+            this.pictureBox3.Location = new System.Drawing.Point(16, 264);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(153, 81);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -101,7 +105,7 @@ namespace DRAG
             // pictureBox2
             // 
             this.pictureBox2.Image = global::DRAG.Properties.Resources.siadełko;
-            this.pictureBox2.Location = new System.Drawing.Point(16, 166);
+            this.pictureBox2.Location = new System.Drawing.Point(16, 148);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(153, 81);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -112,7 +116,7 @@ namespace DRAG
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(16, 39);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 21);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(153, 81);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -123,7 +127,7 @@ namespace DRAG
             // pictureBox4
             // 
             this.pictureBox4.Image = global::DRAG.Properties.Resources.ramar;
-            this.pictureBox4.Location = new System.Drawing.Point(27, 39);
+            this.pictureBox4.Location = new System.Drawing.Point(19, 21);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(138, 81);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -145,22 +149,20 @@ namespace DRAG
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(84, 7);
+            this.label1.Location = new System.Drawing.Point(35, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(126, 29);
             this.label1.TabIndex = 5;
-            this.label1.Text = "label1";
+            this.label1.Text = "Przeciągnij zdjęcia na ich odpowiednie miejsca";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(692, 7);
+            this.label2.Location = new System.Drawing.Point(632, 7);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(129, 29);
             this.label2.TabIndex = 6;
-            this.label2.Text = "label2";
+            this.label2.Text = "Przeciągnij zdjęcia na ich odpowiednie miejsca";
             // 
             // lblDropHere11
             // 
@@ -176,15 +178,15 @@ namespace DRAG
             this.panel2.Controls.Add(this.pictureBox6);
             this.panel2.Controls.Add(this.pictureBox5);
             this.panel2.Controls.Add(this.pictureBox4);
-            this.panel2.Location = new System.Drawing.Point(609, 23);
+            this.panel2.Location = new System.Drawing.Point(616, 41);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(189, 415);
+            this.panel2.Size = new System.Drawing.Size(172, 369);
             this.panel2.TabIndex = 8;
             // 
             // pictureBox6
             // 
             this.pictureBox6.Image = global::DRAG.Properties.Resources.koło_roweru;
-            this.pictureBox6.Location = new System.Drawing.Point(27, 282);
+            this.pictureBox6.Location = new System.Drawing.Point(19, 264);
             this.pictureBox6.Name = "pictureBox6";
             this.pictureBox6.Size = new System.Drawing.Size(138, 81);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -195,7 +197,7 @@ namespace DRAG
             // pictureBox5
             // 
             this.pictureBox5.Image = global::DRAG.Properties.Resources.ped;
-            this.pictureBox5.Location = new System.Drawing.Point(27, 166);
+            this.pictureBox5.Location = new System.Drawing.Point(19, 148);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(138, 81);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -371,11 +373,38 @@ namespace DRAG
             this.lblDropHere15.TabIndex = 24;
             this.lblDropHere15.Text = "Drop Here";
             // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Broadway", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(291, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(244, 38);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Ułóż zdjęcia tak aby powstał pełen przedmiot";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(378, 425);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Level";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.lblDropHere15);
             this.Controls.Add(this.lblDropHere13);
             this.Controls.Add(this.lblDropHere10);
@@ -453,6 +482,9 @@ namespace DRAG
         private System.Windows.Forms.Label lblDropHere10;
         private System.Windows.Forms.Label lblDropHere13;
         private System.Windows.Forms.Label lblDropHere15;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- DRAG/Form1.cs
+++ DRAG/Form1.cs
@@ -12,9 +12,14 @@ namespace DRAG
 {
     public partial class Form1 : Form
     {
+        public bool level1 = true;
+        public bool level2 = false;
+        public bool level3 = false;
+        public bool lvl = false;
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -26,13 +31,51 @@ namespace DRAG
         {
             //pictureBox1.DoDragDrop(pictureBox1.Image, DragDropEffects.Copy);
             ((PictureBox)sender).DoDragDrop(((PictureBox)sender).Image, DragDropEffects.Copy);
+            
         }
 
         private void Gamemode()
         {
-            int level1;
-            int level2;
-            int level3;
+            timer1.Start();
+            
+            if (level1==true)
+            {
+                
+                if (CheckForWinLevel1(level1) == false)
+                {
+                    level1 = false;
+                    level2 = true;
+                    
+                }
+               
+            }
+            if (level2==true)
+            {
+                MessageBox.Show("2 poziom");
+                level2 = false;
+                timer1.Stop();
+            }
+           /* if (level2 == true)
+            {
+                CheckForWinLevel1();
+            }
+            if (level3 == true)
+            {
+                CheckForWinLevel1();
+            }*/
+        }
+
+        private bool CheckForWinLevel1(bool level1)
+        {
+            if (pictureBox10.Image == pictureBox1.Image && pictureBox11.Image == pictureBox4.Image && pictureBox6.Image == pictureBox12.Image && pictureBox5.Image == pictureBox14.Image && pictureBox2.Image == pictureBox8.Image && pictureBox9.Image == pictureBox3.Image)
+            {
+                MessageBox.Show("Congratulations");
+                return false;
+
+            }
+            else return true;
+            
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -47,7 +90,7 @@ namespace DRAG
             pictureBox13.AllowDrop = true;
             pictureBox14.AllowDrop = true;
             pictureBox15.AllowDrop = true;
-
+            Gamemode();
             lblDropHere7.Visible = false;
             lblDropHere8.Visible = false;
             lblDropHere9.Visible = false;
@@ -80,7 +123,7 @@ namespace DRAG
         }
         private void pictureBox8_DragEnter(object sender, DragEventArgs e)
         {
-            lblDropHere8.Visible = true;
+            lblDropHere7.Visible = true;
             if (e.Data.GetDataPresent(DataFormats.Bitmap))
             {
                 e.Effect = DragDropEffects.Copy;
@@ -88,11 +131,11 @@ namespace DRAG
         }
         private void pictureBox8_DragLeave(object sender, EventArgs e)
         {
-            lblDropHere8.Visible = false;
+            lblDropHere7.Visible = false;
         }
         private void pictureBox8_DragDrop(object sender, DragEventArgs e)
         {
-            lblDropHere8.Visible = false;
+            lblDropHere7.Visible = false;
             Image getPicture = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
             pictureBox8.Image = getPicture;
         }
@@ -223,5 +266,20 @@ namespace DRAG
             pictureBox15.Image = getPicture;
         }
 
+       
+
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Gamemode();
+            
+            /*if (level1 == true)
+            {
+                timer1.Start();
+            }
+            else if (level1 == false) timer1.Stop(); */
+        }
     }
 }
